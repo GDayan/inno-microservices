e0ba96b7cb242b6c2509458391d98c4ae88ed852
Dayan Gutlyev <daykanot@gmail.com>
2025-09-22 11:17:06 +0300

    USR-2: add model, repository, and service classes

---
+++ userService/src/main/java/com/innowise/model/dto/CardDto.java
@@ -0,0 +1,10 @@
+package com.innowise.model.dto;
+
+import java.time.LocalDate;
+
+public class CardDto {
+    private Long id;
+    private String number;
+    private String holder;
+    private LocalDate expirationDate;
+}
---
+++ userService/src/main/java/com/innowise/model/entity/Card.java
@@ -0,0 +1,29 @@
+package com.innowise.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "card_info")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+
+    @Column(nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
+
